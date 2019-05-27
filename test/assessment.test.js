c30c66f13ea0a5f7ec9fcafa81755d6fd0729ce4
Sumedh Nimkarde <blesssumedh@gmail.com>
2019-05-27 11:28:50 +0530

    :white_check_mark: Add tests for the Assessment

--- test/assessment.test.js
+++ test/assessment.test.js
@@ -1,25 +1,98 @@
-import Assessment from '../src/assessment'
+import Assessment from "../src/assessment";
+import Dimensions from "../src/dimensions";
 
-// Feel free to rewrite this test suite. This is provided as guidance.
-describe('The Assessment', () => {
-  it('should have 30 questions', () => {
-    expect('this test').toBe('failing');
+import * as _ from "underscore";
+
+describe("The Assessment", () => {
+  let assessment, questions;
+  beforeEach(async () => {
+    assessment = new Assessment();
+    await assessment.populateQuestions();
+    questions = assessment.questions;
   });
-  it('should not show the same answer twice', () => {
-    expect('this test').toBe('failing');   
+
+  it("should have 30 questions", async () => {
+    expect(assessment.questions).toHaveLength(30);
   });
-  it('should match each dimension to the other dimensions exactly 2 times', () => {
-    expect('this test').toBe('failing');   
+
+  it("should not show the same answer twice", async () => {
+    for (let i = 0; i < questions.length; i++) {
+      for (let j = 0; j < questions.length; j++) {
+        if (i !== j) {
+          expect(
+            _.difference(questions[i].options, questions[j].options).length
+          ).not.toBe(0);
+        }
+      }
+    }
   });
-  it('should provide ipsative questions (two possible answers)', () => {
-    expect('this test').toBe('failing');   
+
+  it("should match each dimension to the other dimensions exactly 2 times", async () => {
+    for (let i = 0; i < questions.length; i++) {
+      let count = 1;
+      for (let j = 0; j < questions.length; j++) {
+        if (
+          _.difference(questions[i].combination, questions[j].combination)
+            .length === 0 &&
+          i !== j
+        ) {
+          count++;
+        }
+      }
+      expect(count).toBe(2);
+    }
   });
-  describe('when completed', () => {
-    it('should provide the results as an object', () => {
-      expect('this test').toBe('failing');   
+
+  it("should provide ipsative questions (two possible answers)", async () => {
+    for (let question of questions) {
+      expect(question.options).toHaveLength(2);
+    }
+  });
+
+  describe("when completed", async () => {
+    it("should provide the results as an object", async () => {
+      assessment.chooseAnswer("Adaptive");
+      assessment.chooseAnswer("Adaptive");
+      assessment.chooseAnswer("Adaptive");
+      assessment.chooseAnswer("Adaptive");
+
+      assessment.chooseAnswer("Integrity");
+      assessment.chooseAnswer("Integrity");
+      assessment.chooseAnswer("Integrity");
+      assessment.chooseAnswer("Integrity");
+
+      assessment.chooseAnswer("Customer");
+      assessment.chooseAnswer("Customer");
+      assessment.chooseAnswer("Customer");
+
+      assessment.chooseAnswer("Detail");
+
+      assessment.chooseAnswer("Result");
+
+      assessment.chooseAnswer("Collaborative");
+
+      const results = await assessment.getTestResults();
+
+      expect(results).toBeInstanceOf(Object);
+      expect(results["Adaptive"]).toBe(4);
+      expect(results["Integrity"]).toBe(4);
+      expect(results["Customer"]).toBe(3);
+      expect(results["Detail"]).toBe(1);
+      expect(results["Result"]).toBe(1);
+      expect(results["Collaborative"]).toBe(1);
     });
-    it('should represent the results based on 6 dimensions', () => {
-      expect('this test').toBe('failing');   
+
+    it("should represent the results based on 6 dimensions", async () => {
+      const results = await assessment.getTestResults();
+      const dimensions = Object.keys(results);
+
+      expect(dimensions).toHaveLength(6);
+      expect(dimensions).toContain("Adaptive");
+      expect(dimensions).toContain("Integrity");
+      expect(dimensions).toContain("Result");
+      expect(dimensions).toContain("Collaborative");
+      expect(dimensions).toContain("Detail");
+      expect(dimensions).toContain("Customer");
     });
   });
 });
